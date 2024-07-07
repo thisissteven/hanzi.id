927667815f49df4914d986d785d03296268d6545
steven2801 <ssteven075@gmail.com>
2024-07-07 16:05:47 +0700

    remove

--- pages/typing-test.tsx
+++ pages/typing-test.tsx
@@ -1,10 +1,5 @@
-import { HanziStrokeSimulator } from "@/components";
 import React from "react";
 
 export default function TypingTest() {
-  return (
-    <div>
-      <HanziStrokeSimulator />
-    </div>
-  );
+  return <div></div>;
 }
