6d6d1a057d7174f9dfc55d02243e5db644f9dda6
steven2801 <ssteven075@gmail.com>
2024-07-13 16:42:43 +0700

    temp

--- modules/flashcards/review/character-card.tsx
+++ modules/flashcards/review/character-card.tsx
@@ -1,5 +1,6 @@
-import clsx from "clsx";
 import React from "react";
+import { CardStatus } from "./content";
+import { cn } from "@/utils";
 
 export type Locale = "en" | "id";
 
@@ -10,6 +11,7 @@ export function CharacterCard({
   translations,
   isFlipped,
   className,
+  status,
 }: {
   id: number;
   character: string;
@@ -17,6 +19,7 @@ export function CharacterCard({
   pinyin: string;
   isFlipped: boolean;
   className: string;
+  status: CardStatus;
 }) {
   return (
     <>
@@ -39,20 +42,33 @@ export function CharacterCard({
           transform: rotateY(180deg);
         }
       `}</style>
-      <div className={clsx("select-none text-4xl aspect-square text-smokewhite duration-200", className)}>
-        <div className={clsx("relative w-full h-full card", isFlipped && "card-flipped")}>
+      <div className={cn("select-none aspect-square text-smokewhite duration-200", className)}>
+        <div className={cn("relative w-full h-full card", isFlipped && "card-flipped")}>
           <div
-            className={clsx(
+            className={cn(
               "card-content has-[input:active]:scale-[98%] transition absolute inset-0 border-b-4 grid place-items-center bg-softblack border-secondary/10 rounded-lg overflow-hidden"
             )}
           >
-            <span className="font-medium">{character}</span>
+            <span className="font-medium text-4xl sm:text-5xl">{character}</span>
 
             <div className="absolute left-4 top-4 text-sm">{id}</div>
+
+            <div className="absolute right-4 top-4 text-sm">
+              <span
+                className={cn(
+                  "inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset duration-200",
+                  status === "wrong" && "bg-red-400/10 text-red-400 ring-red-400/20",
+                  status === "correct" && "bg-emerald-400/10 text-emerald-400 ring-emerald-400/20",
+                  status === "untouched" && "opacity-0"
+                )}
+              >
+                {status}
+              </span>
+            </div>
           </div>
           <div
-            className={clsx(
-              "card-content absolute inset-0 border-b-4 bg-softblack flex flex-col items-center justify-center text-flipped px-4 border-secondary/10 rounded-lg overflow-hidden"
+            className={cn(
+              "card-content absolute inset-0 border-b-4 bg-softblack flex flex-col items-center justify-center text-flipped px-4 border-secondary/10 rounded-lg overflow-hidden text-center"
             )}
           >
             <div className="text-3xl">{pinyin}</div>
--- modules/flashcards/review/content.tsx
+++ modules/flashcards/review/content.tsx
@@ -1,7 +1,10 @@
 import { usePreferences } from "@/components";
-import { FlashcardedResult, FlashcardedResultStrict } from "@/pages/api/flashcard/en";
+import { FlashcardedResultStrict } from "@/pages/api/flashcard/en";
 import React from "react";
 import { CharacterCard } from "./character-card";
+import { LucideCheck, LucideEye, LucideEyeOff, LucideX } from "lucide-react";
+
+export type CardStatus = "wrong" | "correct" | "untouched";
 
 export function FlashcardReviewContent({ words }: { words: FlashcardedResultStrict[] }) {
   const [currentIndex, setCurrentIndex] = React.useState(0);
@@ -14,32 +17,61 @@ export function FlashcardReviewContent({ words }: { words: FlashcardedResultStri
   const cardRefs = React.useRef([]) as React.MutableRefObject<(HTMLDivElement | null)[]>;
 
   React.useEffect(() => {
-    if (cardRefs.current[currentIndex]) {
-      cardRefs.current[currentIndex].scrollIntoView({
-        behavior: "smooth",
-        block: "nearest",
-        inline: "center",
-      });
+    if (cardRefs.current) {
+      const currentCard = cardRefs.current[currentIndex];
+      if (currentCard) {
+        currentCard.scrollIntoView({
+          behavior: "smooth",
+          block: "nearest",
+          inline: "center",
+        });
+      }
     }
   }, [currentIndex]);
 
+  const [reviewResult, setReviewResult] = React.useState<Array<CardStatus>>(
+    Array.from({ length: words.length }, () => "untouched")
+  );
+
+  const wrong = reviewResult.filter((result) => result === "wrong").length;
+  const correct = reviewResult.filter((result) => result === "correct").length;
+  const untouched = reviewResult.length - wrong - correct;
+
   return (
-    <div className="flex flex-col items-center justify-center h-[calc(100vh-11.25rem)]">
-      <div className="w-full overflow-x-auto flex gap-2 scrollbar-none pointer-events-none" ref={containerRef}>
+    <div className="max-sm:px-2 flex flex-col items-center justify-center min-h-[calc(100dvh-5.5rem)] md:min-h-[calc(100dvh-12.5rem)]">
+      <div className="px-2 w-full flex flex-wrap gap-4 justify-center">
+        <div className="flex gap-2 items-center text-sm">
+          <div className="w-2 h-2 rounded-full bg-red-500"></div> {wrong} Wrong
+        </div>
+        <div className="flex gap-2 items-center text-sm">
+          <div className="w-2 h-2 rounded-full bg-emerald-500"></div> {correct} Correct
+        </div>
+        <div className="flex gap-2 items-center text-sm">
+          <div className="w-2 h-2 rounded-full bg-gray-500"></div> {untouched} Untouched
+        </div>
+      </div>
+      <div className="mt-4 w-full overflow-x-auto flex gap-2 scrollbar-none pointer-events-none" ref={containerRef}>
         {words.map((word, index) => {
           const character = isSimplified ? word.simplified : word.traditional;
           const pinyin = word.entries.map((entry) => entry.pinyin).join(", ");
           const translations = word.entries[0].english.join(", ");
           const active = index === currentIndex;
+
+          const style = {
+            marginLeft: index === 0 ? "50%" : undefined,
+            marginRight: index === words.length - 1 ? "50%" : undefined,
+          };
+
           return (
             <div
               key={word.simplified}
-              className="shrink-0 w-full max-sm:px-2 sm:max-w-80 aspect-square"
+              className="shrink-0 w-full max-w-96 sm:max-w-80 aspect-square"
               ref={(el) => {
                 if (el) {
                   cardRefs.current[index] = el;
                 }
               }}
+              style={style}
             >
               <CharacterCard
                 id={index + 1}
@@ -48,25 +80,78 @@ export function FlashcardReviewContent({ words }: { words: FlashcardedResultStri
                 translations={translations}
                 isFlipped={index === flipped}
                 className={active ? "opacity-100" : "opacity-40 scale-90"}
+                status={reviewResult[index]}
               />
             </div>
           );
         })}
       </div>
-      <div className="mt-4 flex space-x-4">
+
+      <div className="mt-4 flex flex-wrap gap-2 w-full max-w-80 mx-auto">
         <button
+          onClick={() => {
+            setReviewResult((prev) => {
+              const newResult = [...prev];
+              newResult[currentIndex] = "wrong";
+              return newResult;
+            });
+            setCurrentIndex((prev) => {
+              const nextIndex = Math.min(words.length - 1, prev + 1);
+              if (reviewResult[prev] !== "untouched") {
+                return prev;
+              }
+              setFlipped(null);
+              return nextIndex;
+            });
+          }}
+          className="flex items-center rounded-md duration-200 active:bg-red-400/20 active:border-red-400/20 bg-red-400/10 text-red-400 border-b-2 border-red-400/20 p-3"
+        >
+          <LucideX />
+        </button>
+
+        <button
+          className="grid place-items-center rounded-md duration-200 active:bg-sky-400/20 active:border-sky-400/20 bg-sky-400/10 text-sky-400 border-b-2 border-sky-400/20 p-3 flex-1"
+          onClick={() => setFlipped(currentIndex === flipped ? null : currentIndex)}
+        >
+          {flipped !== null ? <LucideEyeOff /> : <LucideEye />}
+        </button>
+
+        <button
+          onClick={() => {
+            setReviewResult((prev) => {
+              const newResult = [...prev];
+              newResult[currentIndex] = "correct";
+              return newResult;
+            });
+            setCurrentIndex((prev) => {
+              const nextIndex = Math.min(words.length - 1, prev + 1);
+              if (reviewResult[prev] !== "untouched") {
+                return prev;
+              }
+              setFlipped(null);
+              return nextIndex;
+            });
+          }}
+          className="flex items-center rounded-md duration-200 active:bg-emerald-400/20 active:border-emerald-400/20 bg-emerald-400/10 text-emerald-400 border-b-2 border-emerald-400/20 p-3"
+        >
+          <LucideCheck />
+        </button>
+      </div>
+      <div className="fixed bottom-2 left-1/2 -translate-x-1/2 px-2 flex flex-wrap gap-2 w-full max-w-80">
+        <button
+          disabled={currentIndex === 0}
           onClick={() => setCurrentIndex((prev) => Math.max(0, prev - 1))}
-          className="px-4 py-2 bg-softblack text-smokewhite rounded-md font-medium duration-200 active:bg-subtle"
+          className="flex-1 whitespace-nowrap grid place-items-center bg-subtle/50 border-b-2 border-secondary/10 rounded-md active:bg-hovered duration-200 p-3 disabled:opacity-50 disabled:pointer-events-none"
         >
-          Previous
+          &#8592; Previous
         </button>
         <button
+          disabled={currentIndex === words.length - 1 || reviewResult[currentIndex] === "untouched"}
           onClick={() => setCurrentIndex((prev) => Math.min(words.length - 1, prev + 1))}
-          className="px-4 py-2 bg-softblack text-smokewhite rounded-md font-medium duration-200 active:bg-subtle"
+          className="flex-1 whitespace-nowrap grid place-items-center bg-subtle/50 border-b-2 border-secondary/10 rounded-md active:bg-hovered duration-200 p-3 disabled:opacity-50 disabled:pointer-events-none"
         >
-          Next
+          Next &#x2192;
         </button>
-        <button onClick={() => setFlipped(currentIndex === flipped ? null : currentIndex)}>flip</button>
       </div>
     </div>
   );
