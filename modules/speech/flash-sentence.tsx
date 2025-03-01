f1f3a01f44c668d49a43a0f60e830b1ffa4e36bc
Steven <ssteven075@gmail.com>
2025-03-01 16:53:18 +0700

    update

--- modules/speech/flash-sentence.tsx
+++ modules/speech/flash-sentence.tsx
@@ -18,7 +18,7 @@ export function FlashSentence({ sentence, wordRange, currentSentenceIdx, mode, i
       <div className="fixed left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
         <div className="grid place-items-center gap-2">
           <div className="w-36 h-px bg-smoke"></div>
-          <span className="absolute left-1/2 -translate-x-1/2 whitespace-nowrap text-neutral-50">{currentWord}</span>
+          <div className="absolute left-1/2 -translate-x-1/2 whitespace-nowrap text-neutral-50">{currentWord}</div>
           <div className="opacity-0" aria-hidden>
             行
           </div>
