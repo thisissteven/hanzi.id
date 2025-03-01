39597ce0849c8c05d86f377e8e18dd680ff3a64d
Steven <ssteven075@gmail.com>
2025-03-01 17:35:37 +0700

    update

--- modules/speech/flash-sentence.tsx
+++ modules/speech/flash-sentence.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@/utils";
+
 type CurrentSentenceProps = {
   sentence: string;
   wordRange: number[];
@@ -6,7 +8,7 @@ type CurrentSentenceProps = {
   isPlaying: boolean;
 };
 
-export function FlashSentence({ sentence, wordRange, currentSentenceIdx, mode, isPlaying }: CurrentSentenceProps) {
+export function FlashSentence({ sentence, wordRange, mode, isPlaying }: CurrentSentenceProps) {
   const [start, end] = wordRange;
 
   const currentWord = sentence.slice(start, end);
@@ -18,9 +20,8 @@ export function FlashSentence({ sentence, wordRange, currentSentenceIdx, mode, i
       <div className="fixed left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
         <div className="grid place-items-center gap-2">
           <div className="w-36 h-px bg-smoke"></div>
-          <div className="absolute left-1/2 -translate-x-1/2 whitespace-nowrap text-neutral-50">{currentWord}</div>
-          <div className="opacity-0" aria-hidden>
-            行
+          <div className={cn("whitespace-nowrap text-neutral-50", !currentWord && "opacity-0")}>
+            {currentWord ? currentWord : "词"}
           </div>
           <div className="w-36 h-px bg-smoke"></div>
         </div>
